82d8ba9e5461dccab89aab0815d514a8e7cdecc9
Evan Smith <evan.leigh@outlook.com>
2024-10-02 19:52:57 -0400

    fix: added `prisma` back in

--- src/app/api/datasets/[id]/route.ts
+++ src/app/api/datasets/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '@/auth';
+import prisma from '@/lib/prisma';
 import { deleteDataset } from '@/actions/dataset';
 
 export async function GET(_request: NextRequest, { params }: { params: { id: string } }) {
